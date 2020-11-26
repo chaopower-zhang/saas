56d9ba5544229972ce9dbf733e5b5a6e2e453ffe
chaopower-zhang <991898441@qq.com>
2020-11-26 10:41:01 +0800

    添加项目模态框

--- app01/middleware/auth.py
+++ app01/middleware/auth.py
@@ -5,20 +5,61 @@ from django.utils.deprecation import MiddlewareMixin
 from app01 import models
 from django.shortcuts import redirect
 from django.conf import settings
+import datetime
+
+
+# 封装处理
+class Tracer(object):
+
+    def __init__(self):
+        self.user = None
+        self.price_policy = None
+        self.project = None
 
 
 class AuthMiddleware(MiddlewareMixin):
 
     def process_request(self, request):
-        # request.tracer = Tracer()
+        request.tracer = Tracer()
         user_id = request.session.get('user_id', 0)
         user_object = models.UserInfo.objects.filter(id=user_id).first()
-        request.tracer = user_object
+        request.tracer.user = user_object
 
         # 白名单 没有登陆都可以访问的页面
         # 获取url,是否在白名单中，不在返回登陆
         if request.path_info in settings.WHITE_REGEX_URL_LIST:
             return
 
-        if not request.tracer:
+        if not request.tracer.user:
             return redirect('app01:login')
+
+        # 登陆成功之后，已登陆，获取当前用户的所有的额度
+
+        # 方式一： 免费的额度在交易记录表中存储
+
+        # 获取当前用户ID值最大（最近交易记录）
+        _object = models.Transaction.objects.filter(user=user_object, status=2, price_policy__category=1).order_by('-id').first()
+
+        # 判断是否过期
+        current_datatime = datetime.datetime.now()
+        if _object.end_datetime and _object.end_datetime < current_datatime:
+            _object = models.Transaction.objects.filter(user=user_object, status=2, price_policy__category=1).first()
+
+        request.tracer.price_policy = _object.price_policy
+
+        # 方式二
+        # 获取当前用户ID值最大（最近交易记录）
+        """
+        _object = models.Transaction.objects.filter(user=user_object, status=2).order_by('-id').first()
+
+        if not _object:
+            # 没有购买
+            request.tracer.price_policy = models.PricePolicy.objects.filter(category=1, title="个人免费版").first()
+        else:
+            # 付费版
+            current_datetime = datetime.datetime.now()
+            if _object.end_datetime and _object.end_datetime < current_datetime:
+                request.tracer.price_policy = models.PricePolicy.objects.filter(category=1, title="个人免费版").first()
+            else:
+                request.tracer.price_policy = _object.price_policy
+        """
--- app01/views/project.py
+++ app01/views/project.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render, HttpResponse, redirect
-
+from app01.forms.project import ProjectModelForm
 
 def project_list(request):
-    return render(request, 'project_list.html')
+
+    form = ProjectModelForm(request)
+
+    return render(request, 'project_list.html', {'form': form})
