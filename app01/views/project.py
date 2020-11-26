f00e3cb1c2a48cef7dbc6de533c6bf14d17b811a
chaopower-zhang <991898441@qq.com>
2020-11-26 12:53:43 +0800

    添加项目

--- app01/views/project.py
+++ app01/views/project.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render, HttpResponse, redirect
 from app01.forms.project import ProjectModelForm
+from django.http import JsonResponse
+
 
 def project_list(request):
+    if request.method == 'GET':
 
-    form = ProjectModelForm(request)
+        form = ProjectModelForm(request)
 
-    return render(request, 'project_list.html', {'form': form})
+        return render(request, 'project_list.html', {'form': form})
+
+    form = ProjectModelForm(request, data=request.POST)
+    if form.is_valid():
+        form.instance.creator = request.trace.user
+
+    return JsonResponse({'status':False, 'error':form.errors})
