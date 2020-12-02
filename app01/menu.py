33deb774e103c286ecf37cdd307e110b75701f2b
chaopower-zhang <991898441@qq.com>
2020-12-02 22:18:53 +0800

    '添加wiki'

--- app01/menu.py
+++ app01/menu.py
@@ -7,6 +7,8 @@ from app01.views import wiki, file, setting, issues, dashboard, statistics
 
 urlpatterns = [
     url(r'^wiki/$', wiki.wiki, name='wiki'),
+    url(r'^wiki-add/$', wiki.wiki_add, name='wiki_add'),
+    url(r'^wiki-catalog/$', wiki.wiki_catalog, name='wiki_catalog'),
     url(r'^file/$', file.file, name='file'),
     url(r'^setting/$', setting.setting, name='setting'),
     url(r'^issues/$', issues.issues, name='issues'),
