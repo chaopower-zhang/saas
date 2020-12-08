c636a3ac52620a712a77c60a858e019f221f87d7
chaopower-zhang <991898441@qq.com>
2020-12-08 22:21:42 +0800

    '文件夹添加修改'

--- app01/views/file.py
+++ app01/views/file.py
@@ -1,12 +1,67 @@
 from django.shortcuts import render
+from app01.forms.file import FolderModelForm, FileModelForm
+from app01 import models
+from django.forms import model_to_dict
+from django.http import JsonResponse
 
 
+# http://127.0.0.1:8002/manage/1/file/
+# http://127.0.0.1:8002/manage/1/file/?folder=1
 def file(request, project_id):
-    """
-    wiki首页
-    :param request:
-    :param project_id:
-    :return:
-    """
+    """ 文件列表 & 添加文件夹 """
 
-    return render(request, 'file.html')
+    parent_object = None
+    folder_id = request.GET.get('folder', "")
+    if folder_id.isdecimal():
+        parent_object = models.FileRepository.objects.filter(id=int(folder_id), file_type=2,
+                                                             project=request.tracer.project).first()
+
+    # GET 查看页面
+    if request.method == "GET":
+
+        breadcrumb_list = []
+        parent = parent_object
+        while parent:
+            # breadcrumb_list.insert(0, {'id': parent.id, 'name': parent.name})
+            breadcrumb_list.insert(0, model_to_dict(parent, ['id', 'name']))
+            parent = parent.parent
+
+        # 当前目录下所有的文件 & 文件夹获取到即可
+        queryset = models.FileRepository.objects.filter(project=request.tracer.project)
+        if parent_object:
+            # 进入了某目录
+            file_object_list = queryset.filter(parent=parent_object).order_by('-file_type')
+        else:
+            # 根目录
+            file_object_list = queryset.filter(parent__isnull=True).order_by('-file_type')
+        form = FolderModelForm(request, parent_object)
+
+        context = {
+            'form': form,
+            "file_object_list": file_object_list,
+            "breadcrumb_list": breadcrumb_list,
+            'folder_object': parent_object
+        }
+        return render(request, 'file.html', context)
+
+    # POST 添加文件夹 & 文件夹的修改
+    fid = request.POST.get('fid', '')
+    edit_object = None
+    if fid.isdecimal():
+        edit_object = models.FileRepository.objects.filter(id=int(fid), file_type=2,
+                                                           project=request.tracer.project).first()
+
+    if edit_object:
+        form = FolderModelForm(request, parent_object, data=request.POST, instance=edit_object)
+    else:
+        form = FolderModelForm(request, parent_object, data=request.POST)
+
+    if form.is_valid():
+        form.instance.project = request.tracer.project
+        form.instance.file_type = 2
+        form.instance.update_user = request.tracer.user
+        form.instance.parent = parent_object
+        form.save()
+        return JsonResponse({'status': True})
+
+    return JsonResponse({'status': False, 'error': form.errors})
